df16dc15383049ea0eeb2adf317e9866cd8bb01a
Your Name <your_email>
2025-09-19 05:12:36 +0300

    smallfix

--- app/(private routes)/notes/[id]/NoteDetails.client.tsx
+++ app/(private routes)/notes/[id]/NoteDetails.client.tsx
@@ -2,14 +2,15 @@
 
 import { useQuery } from '@tanstack/react-query';
 import css from './page.module.css'
-import { getSingleNote } from '@/lib/api/clientApi';
+// import { getSingleNote } from '@/lib/api/clientApi';
 import { useParams } from 'next/navigation';
+import { fetchNoteById } from '@/lib/api/clientApi';
 
 export default function NoteDetailsClient() {
     const { id } = useParams<{ id: string }>();
     const { data: note, isLoading, error } = useQuery({
         queryKey: ["note", id],
-        queryFn: () => getSingleNote(id),
+        queryFn: () => fetchNoteById(id),
         refetchOnMount: false
     })
     if (isLoading) return <p>Loading, please wait...</p>
